ce6ae949448425470fc58d29f27a727f684f50ca
Seth Axen <seth.axen@gmail.com>
2021-09-20 16:28:29 +0200

    Run formatter

--- src/utils.jl
+++ src/utils.jl
@@ -7,4 +7,4 @@ end
 uniform_probabilities(T, npoints::Integer) = ((1:npoints) .- T(1//2)) ./ npoints
 
 # p-th quartile of x, assuming x is sorted in increasing order
-quartile(x, p) = x[fld(length(x) * p + 2, 4)] 
+quartile(x, p) = x[fld(length(x) * p + 2, 4)]
