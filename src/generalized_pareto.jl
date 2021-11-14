ad27fefaa515c06789330d724025f9b6342c6294
Seth Axen <seth.axen@gmail.com>
2021-11-14 22:22:15 +0100

    Fix bug

--- src/generalized_pareto.jl
+++ src/generalized_pareto.jl
@@ -97,7 +97,7 @@ function fit_empiricalbayes(
         # support is nearly a point. solution is not unique; any solution satisfying the
         # constraints σ/ξ ≈ 0 and ξ < 0 is acceptable. we choose the ξ = -1 solution, i.e.
         # the uniform distribution
-        return DistributionsGeneralizedPareto(μ, max(eps(zero(T)), xmax - μ), -one(μ))
+        return Distributions.GeneralizedPareto(μ, max(eps(zero(T)), xmax - μ), -one(μ))
     end
     # estimate θ using empirical bayes
     θ_hat = _fit_gpd_θ_empirical_bayes(μ, xsorted, min_points, improved)
