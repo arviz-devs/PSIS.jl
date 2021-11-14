eff4ea8e8c1307ab0e57cb09dd30cf44862455c5
Seth Axen <seth.axen@gmail.com>
2021-11-14 21:35:15 +0100

    Use own rng for each loop

--- test/generalized_pareto.jl
+++ test/generalized_pareto.jl
@@ -5,13 +5,13 @@ using Test
 
 @testset "Generalized Pareto distribution" begin
     @testset "fit" begin
-        rng = MersenneTwister(42)
         @testset for μ in (-1, 5),
             σ in (0.5, 1.0, 2.0),
             ξ in (-1.0, 0.0, 0.3, 1.0, 2.0),
             improved in (true, false)
 
             d = GeneralizedPareto(μ, σ, ξ)
+            rng = MersenneTwister(42)
             x = rand(rng, d, 200_000)
             dhat = fit(
                 PSIS.GeneralizedParetoKnownMu(μ), x; min_points=80, improved=improved
