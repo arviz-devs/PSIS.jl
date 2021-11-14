e8a529c6e17f941b1c71fe8a74aa52eaa2b3627a
Seth Axen <seth.axen@gmail.com>
2021-11-14 21:04:25 +0100

    Test for both values of improved

--- test/generalized_pareto.jl
+++ test/generalized_pareto.jl
@@ -6,10 +6,16 @@ using Test
 @testset "Generalized Pareto distribution" begin
     @testset "fit" begin
         Random.seed!(42)
-        @testset for μ in (-1, 5), σ in (0.5, 1.0, 2.0), ξ in (-1.0, 0.0, 0.3, 1.0, 2.0)
+        @testset for μ in (-1, 5),
+            σ in (0.5, 1.0, 2.0),
+            ξ in (-1.0, 0.0, 0.3, 1.0, 2.0),
+            improved in (true, false)
+
             d = GeneralizedPareto(μ, σ, ξ)
             x = rand(d, 200_000)
-            dhat = fit(PSIS.GeneralizedParetoKnownMu(μ), x; min_points=80)
+            dhat = fit(
+                PSIS.GeneralizedParetoKnownMu(μ), x; min_points=80, improved=improved
+            )
             @test dhat.σ ≈ σ atol = 0.01
             @test dhat.ξ ≈ ξ atol = 0.01
         end
