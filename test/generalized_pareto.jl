399ebaf14e09cdf370d60546675692800d612e28
Seth Axen <seth.axen@gmail.com>
2021-11-14 20:56:56 +0100

    Update tests

--- test/generalized_pareto.jl
+++ test/generalized_pareto.jl
@@ -1,19 +1,17 @@
+using Distributions
 using PSIS
 using Random
 using Test
 
 @testset "Generalized Pareto distribution" begin
-    @testset "consistency of quantile and fit" begin
-        # check that when uniform draws from the quantiles are used to fit the parameters,
-        # the original parameters are approximately recovered
+    @testset "fit" begin
         Random.seed!(42)
-        u = rand(200_000)
-        @testset for σ in (0.5, 1.0, 2.0), k in (-1.0, 0.0, 0.3, 1.0, 2.0)
-            d = PSIS.GeneralizedPareto(σ, k)
-            x = PSIS.quantile.(Ref(d), u)
-            dhat = PSIS.fit(PSIS.GeneralizedPareto, x; adjust_prior=false, min_points=80)
+        @testset for μ in (-1, 5), σ in (0.5, 1.0, 2.0), ξ in (-1.0, 0.0, 0.3, 1.0, 2.0)
+            d = GeneralizedPareto(μ, σ, ξ)
+            x = rand(d, 200_000)
+            dhat = fit(PSIS.GeneralizedParetoKnownMu(μ), x; min_points=80)
             @test dhat.σ ≈ σ atol = 0.01
-            @test dhat.k ≈ k atol = 0.01
+            @test dhat.ξ ≈ ξ atol = 0.01
         end
     end
 end
