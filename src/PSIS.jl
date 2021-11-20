72ffd2caf9781e9187eae9c9abc950f890c6f47f
Seth Axen <seth.axen@gmail.com>
2021-11-20 20:55:02 +0100

    Improve Distributions implementations

--- src/PSIS.jl
+++ src/PSIS.jl
@@ -2,7 +2,7 @@ module PSIS
 
 using Distributions: Distributions
 using LinearAlgebra: dot
-using LogExpFunctions: logsumexp
+using LogExpFunctions: logsumexp, softmax!
 using Printf: @sprintf
 using Statistics: mean, median, quantile
 using StatsBase: StatsBase
--- src/utils.jl
+++ src/utils.jl
@@ -1,1 +1,4 @@
-uniform_probabilities(T, npoints::Integer) = ((1:npoints) .- T(1//2)) ./ npoints
+function uniform_probabilities(T, npoints::Integer)
+    pmin = 1 / (2 * T(npoints))
+    return pmin:(1 / T(npoints)):(1 - pmin)
+end
