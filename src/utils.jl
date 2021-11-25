c03d84a68cf8ac11b6a6c46b300539cda7076f4a
Seth Axen <seth.axen@gmail.com>
2021-11-25 16:57:00 -0500

    Add utility functions

--- src/utils.jl
+++ src/utils.jl
@@ -2,3 +2,10 @@ function uniform_probabilities(T, npoints::Integer)
     pmin = 1 / (2 * T(npoints))
     return pmin:(1 / T(npoints)):(1 - pmin)
 end
+
+as_array(x::AbstractArray) = x
+as_array(x) = [x]
+
+missing_to_nan(x::AbstractArray{>:Missing}) = replace(x, missing => NaN)
+missing_to_nan(::Missing) = NaN
+missing_to_nan(x) = x
