96a1770af36815557916c258352b6443ebc21c7d
Seth Axen <seth.axen@gmail.com>
2021-12-04 14:09:33 -0500

    Define plotting functions

--- src/PSIS.jl
+++ src/PSIS.jl
@@ -9,12 +9,13 @@ using Statistics: mean, median, quantile
 using StatsBase: StatsBase
 
 export PSISResult
-export psis, psis!
+export psis, psis!, paretoshapeplot, paretoshapeplot!
 
 include("utils.jl")
 include("generalized_pareto.jl")
 include("core.jl")
 
+include("recipes/definitions.jl")
 include("recipes/plots.jl")
 
 end
---
+++ src/recipes/definitions.jl
@@ -0,0 +1,16 @@
+"""
+    paretoshapeplot(values; showlines=false)
+    paretoshapeplot!(values; showlines=false)
+
+Plot the Pareto shape values of `values`.
+
+`values` may be either an array of log importance ratios, in which case [`psis`](@ref) is
+called, or a [`PSISResult`](@ref).
+
+If `showlines=true`, then horizontal lines indicating the different Pareto shape thresholds
+are drawn.
+
+!!! note
+    Plots.jl must be loaded to use these functions
+"""
+paretoshapeplot, paretoshapeplot!
