add5fc6b5712f389ba5f7a5d436c33d1a896d354
Seth Axen <seth.axen@gmail.com>
2021-12-08 15:34:10 -0800

    Test plotting backend

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 @testset "PSIS.jl" begin
     include("generalized_pareto.jl")
     include("core.jl")
+    include("plotting_backend.jl")
     include("plots.jl")
     include("makie.jl")
 end
