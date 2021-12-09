47027693b8bad3fba0343d4a2fae525404d46c21
Seth Axen <seth.axen@gmail.com>
2021-12-08 20:49:05 -0800

    Prefix all function calls with module

--- src/recipes/makie.jl
+++ src/recipes/makie.jl
@@ -10,9 +10,9 @@ using ..Makie
 
 # define an internal plotting recipe for shape values
 @recipe(ParetoShapePlot, pareto_shape) do scene
-    l_theme = default_theme(scene, LineSegments)
-    return Attributes(;
-        default_theme(scene, Scatter)...,
+    l_theme = Makie.default_theme(scene, Makie.LineSegments)
+    return Makie.Attributes(;
+        Makie.default_theme(scene, Makie.Scatter)...,
         linewidth=l_theme.linewidth,
         linecolor=(:grey, 0.7),
         showlines=false,
@@ -44,7 +44,7 @@ function Makie.plot!(p::ParetoShapePlot)
     thresholds = (0, 0.5, 0.7, 1)
     linestyles = (:dot, :dashdot, :dash, :solid)
     for (thresh, linestyle) in zip(thresholds, linestyles)
-        linesegments!(
+        Makie.linesegments!(
             p,
             xminmax,
             fill(thresh, 2);
@@ -55,7 +55,7 @@ function Makie.plot!(p::ParetoShapePlot)
         )
     end
     ks = filter(∉(lattr), keys(attrs))
-    return scatter!(p, p[1]; (k => attrs[k] for k in ks)...)
+    return Makie.scatter!(p, p[1]; (k => attrs[k] for k in ks)...)
 end
 
 # designate default plot type for PSISResult
@@ -63,16 +63,18 @@ Makie.plottype(::PSISResult) = ParetoShapePlot
 
 # convert PSISResult to its shape values when plotted
 function Makie.convert_arguments(P::Makie.PlotFunc, result::PSISResult)
-    return convert_arguments(P, as_array(missing_to_nan(pareto_shape(result))))
+    return Makie.convert_arguments(P, as_array(missing_to_nan(pareto_shape(result))))
 end
 function Makie.convert_arguments(
     ::Type{<:ParetoShapePlot}, pareto_shape::Union{Real,AbstractVector{<:Real}}
 )
-    return convert_arguments(Scatter, as_array(pareto_shape))
+    return Makie.convert_arguments(Makie.Scatter, as_array(pareto_shape))
 end
 
 # set default labels when an Axis is available
-function Makie.plot!(ax::Axis, P::Type{<:ParetoShapePlot}, attributes::Attributes, args...)
+function Makie.plot!(
+    ax::Makie.Axis, P::Type{<:ParetoShapePlot}, attributes::Makie.Attributes, args...
+)
     plt = invoke(
         Makie.plot!,
         Tuple{typeof(ax),Makie.PlotFunc,typeof(attributes),typeof.(args)...},
@@ -85,7 +87,9 @@ function Makie.plot!(ax::Axis, P::Type{<:ParetoShapePlot}, attributes::Attribute
     return plt
 end
 # dispatch on cases like plot(::PSISResult)
-function Makie.plot!(ax::Axis, P::Type{Any}, attributes::Attributes, result::PSISResult)
+function Makie.plot!(
+    ax::Makie.Axis, P::Type{Any}, attributes::Makie.Attributes, result::PSISResult
+)
     plt = Makie.plot!(ax, ParetoShapePlot, attributes, result)
     _set_default_makie_axis_labels!(ax)
     return plt
