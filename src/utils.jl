8c21d7ab27b1e239021f066d9753b572328615f2
Seth Axen <seth.axen@gmail.com>
2021-12-23 20:43:24 +0100

    Add utility functions

--- src/utils.jl
+++ src/utils.jl
@@ -9,3 +9,17 @@ as_array(x) = [x]
 missing_to_nan(x::AbstractArray{>:Missing}) = replace(x, missing => NaN)
 missing_to_nan(::Missing) = NaN
 missing_to_nan(x) = x
+
+# dimension corresponding to parameters
+param_dim(x) = 1
+
+# view of first draw from first chain (i.e. vector of parameters)
+function first_draw(x::AbstractArray)
+    d = ndims(x)
+    @assert d > 1
+    dims = Base.setindex(ntuple(one, d), :, param_dim(x))
+    return view(x, dims...)
+end
+
+# view of all draws 
+param_draws(x::AbstractArray, i::Int) = selectdim(x, param_dim(x), i)
