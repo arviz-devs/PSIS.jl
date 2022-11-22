4f9ad7dc1ed328b8d8c3a785da36ed628ab0b970
Seth Axen <seth@sethaxen.com>
2022-11-22 23:26:22 +0100

    Remove plotting backends

--- src/PSIS.jl
+++ src/PSIS.jl
@@ -8,22 +8,14 @@ using Statistics: Statistics
 
 export PSISResult
 export psis, psis!, ess_is
-export ParetoShapePlot, paretoshapeplot, paretoshapeplot!
-
-const PLOTTING_BACKEND = Ref(:Plots)
 
 include("utils.jl")
 include("generalized_pareto.jl")
 include("core.jl")
 include("ess.jl")
-include("recipes/definitions.jl")
 include("recipes/plots.jl")
 
 function __init__()
-    Requires.@require Makie = "ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" begin
-        using .Makie
-        include("recipes/makie.jl")
-    end
     Requires.@require Distributions = "31c24e10-a181-5473-b8eb-7969acd0382f" begin
         using .Distributions: Distributions
         include("distributions.jl")
