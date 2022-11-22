0c75d97e2d2aad75fc87a025e207d47f1b768b80
Seth Axen <seth@sethaxen.com>
2022-11-22 23:28:01 +0100

    Remove conversion to Distributions types

--- src/PSIS.jl
+++ src/PSIS.jl
@@ -2,7 +2,6 @@ module PSIS
 
 using LogExpFunctions: LogExpFunctions
 using Printf: @sprintf
-using Requires: Requires
 using Statistics: Statistics
 
 export PSISResult
@@ -13,12 +12,5 @@ include("generalized_pareto.jl")
 include("core.jl")
 include("ess.jl")
 include("recipes/plots.jl")
-
-function __init__()
-    Requires.@require Distributions = "31c24e10-a181-5473-b8eb-7969acd0382f" begin
-        using .Distributions: Distributions
-        include("distributions.jl")
-    end
-end
 
 end
