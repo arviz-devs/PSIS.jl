ee2a35ff59d86f741d2be200805572916d7e15b8
Seth Axen <seth@sethaxen.com>
2023-04-07 23:41:19 +0200

    Update test references (#44)
    * Update test references
    
    * Update show test
    
    * Run formatter
    
    * Increment patch number
    
    * Set new Distributions lower bound
    
    * Bump minimum Julia version to v1.4
    
    * Bump minimum Julia version to v1.6

--- test/core.jl
+++ test/core.jl
@@ -84,10 +84,10 @@ using AxisArrays: AxisArrays
                 PSISResult with 100 draws, 1 chains, and 30 parameters
                 Pareto shape (k) diagnostic values:
                                         Count       Min. ESS
-                 (-Inf, 0.5]  good       4 (13.3%)  95
-                  (0.5, 0.7]  okay       2 (6.7%)   97
+                 (-Inf, 0.5]  good       2 (6.7%)   98
+                  (0.5, 0.7]  okay       6 (20.0%)  92
                     (0.7, 1]  bad        4 (13.3%)  ——
-                    (1, Inf)  very bad  19 (63.3%)  ——
+                    (1, Inf)  very bad  17 (56.7%)  ——
                           ——  missing    1 (3.3%)   ——"""
         end
     end
@@ -276,11 +276,8 @@ end
                 Dict("log_weights" => logw, "pareto_shape" => result.pareto_shape),
                 by =
                     (ref, x) ->
-                        isapprox(
-                            permutedims(ref["log_weights"], (2, 3, 1)),
-                            x["log_weights"];
-                            rtol=1e-6,
-                        ) && isapprox(ref["pareto_shape"], x["pareto_shape"]; rtol=1e-6),
+                        isapprox(ref["log_weights"], x["log_weights"]; rtol=1e-6) &&
+                            isapprox(ref["pareto_shape"], x["pareto_shape"]; rtol=1e-6),
             )
         end
     end
