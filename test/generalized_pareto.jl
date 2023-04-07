7ed9393f9a920218d575e762883903621467b79c
Seth Axen <seth@sethaxen.com>
2023-04-08 00:02:30 +0200

    Remove `improved` and `sorted` keywords (#45)
    * Remove improved and sorted keywords
    
    * Remove sorted tests
    
    * Remove improved tests
    
    * Increment minor version

--- test/generalized_pareto.jl
+++ test/generalized_pareto.jl
@@ -5,15 +5,11 @@ using Test
 
 @testset "Generalized Pareto distribution" begin
     @testset "fit_gpd" begin
-        @testset for μ in (-1, 5),
-            σ in (0.5, 1.0, 2.0),
-            k in (-1.0, 0.0, 0.3, 1.0, 2.0),
-            improved in (true, false)
-
+        @testset for μ in (-1, 5), σ in (0.5, 1.0, 2.0), k in (-1.0, 0.0, 0.3, 1.0, 2.0)
             d = Distributions.GeneralizedPareto(μ, σ, k)
             rng = MersenneTwister(42)
             x = rand(rng, d, 200_000)
-            dhat = PSIS.fit_gpd(x; μ=μ, min_points=80, improved=improved)
+            dhat = PSIS.fit_gpd(x; μ=μ, min_points=80)
             @test dhat.μ == μ
             @test dhat.σ ≈ σ atol = 0.01
             @test dhat.k ≈ k atol = 0.01
