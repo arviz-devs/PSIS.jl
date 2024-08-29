e2ec0c3f7c9b8dff023ebe07cd71d1d7152a2b7d
Seth Axen <seth@sethaxen.com>
2024-08-29 11:06:07 +0200

    Use StableRNGs in tests

--- test/generalized_pareto.jl
+++ test/generalized_pareto.jl
@@ -1,13 +1,13 @@
 using Distributions
 using PSIS
-using Random
+using StableRNGs
 using Test
 
 @testset "Generalized Pareto distribution" begin
     @testset "fit_gpd" begin
         @testset for μ in (-1, 5), σ in (0.5, 1.0, 2.0), k in (-1.0, 0.0, 0.3, 1.0, 2.0)
             d = Distributions.GeneralizedPareto(μ, σ, k)
-            rng = MersenneTwister(42)
+            rng = StableRNG(42)
             x = rand(rng, d, 200_000)
             dhat = PSIS.fit_gpd(x; μ=μ, min_points=80)
             @test dhat.μ == μ
